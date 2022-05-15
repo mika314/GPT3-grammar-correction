cb4ce6385e7937513c779bbd481254300f2bf6a6
Mika Pi <mika@mika.global>
2022-05-15 10:38:45 -0700

    Provide a different prompt so that GPT-3 can correct a wider range of mistakes

--- grammar-correction.sh
+++ grammar-correction.sh
@@ -16,7 +16,7 @@ fi
 jsonOutput=$(curl https://api.openai.com/v1/engines/text-davinci-002/completions \
                   -H "Authorization: Bearer $OPENAI_API_KEY" \
                   -H "Content-Type: application/json" \
-                  -d "{\"prompt\": \"Correct this to GenZ English:\\n\\n$selection\",\"max_tokens\": 60,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0}" 2> /dev/null)
+                  -d "{\"prompt\": \"Correct this:\\n\\n$selection\\nCorrected:\",\"max_tokens\": 60,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0}" 2> /dev/null)
 
 # Extract the result from jsonOutput.
 result=$(echo $jsonOutput | jq -r '.choices[0].text')
