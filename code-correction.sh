543d54ea2ce8ffca485f6e24ff23614b5616d154
Mika Pi <mika@mika.global>
2022-05-25 17:01:41 -0700

    Escape stars

--- code-correction.sh
+++ code-correction.sh
@@ -2,11 +2,13 @@
 
 source ~/.gpt3/key.sh
 
+play /usr/lib/libreoffice/share/gallery/sounds/apert.wav &
+
 # Put the selected text into a variable.
 selection=$(xsel -o)
 
 # JSON escape
-selection=$(echo $selection | sed 's/\(["\]\)/\\\1/g')
+selection=$(echo "$selection" | sed 's/\(["\]\)/\\\1/g')
 
 # If nothing is selected, quit.
 if [ -z "$selection" ]; then
@@ -16,17 +18,13 @@ fi
 jsonOutput=$(curl https://api.openai.com/v1/engines/code-davinci-002/completions \
                   -H "Authorization: Bearer $OPENAI_API_KEY" \
                   -H "Content-Type: application/json" \
-                  -d "{\"prompt\": \"Correct C++ code if needed:\\n\\n$selection\\nCorrected:\",\"max_tokens\": 3000,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0}" 2> /dev/null)
+                  -d "{\"prompt\": \"Correct code if needed:\\n\\n${selection//$'\n'/\\n}\\nCorrected:\",\"max_tokens\": 3000,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0}" 2> /dev/null)
 
 # Extract the result from jsonOutput.
-result=$(echo $jsonOutput | jq -r '.choices[0].text')
-echo $result
+result=$(echo "$jsonOutput" | jq -r '.choices[0].text')
 
 # Remove leading carriage returns.
-result=$(echo $result | sed 's/^\^//g')
-
-# Escape result for xdotool
-result=$(echo $result | sed 's/\(["\]\)/\\\1/g')
+result=$(echo "$result" | sed 's/^\^//g')
 
 # Paste using xdotool.
 xdotool type --delay 25 --clearmodifiers "$result"
--- grammar-correction.sh
+++ grammar-correction.sh
@@ -2,11 +2,14 @@
 
 source ~/.gpt3/key.sh
 
+play /usr/lib/libreoffice/share/gallery/sounds/apert.wav &
+
 # Put the selected text into a variable.
 selection=$(xsel -o)
 
 # JSON escape
-selection=$(echo $selection | sed 's/\(["\]\)/\\\1/g')
+selection=$(echo "$selection" | sed 's/\(["\]\)/\\\1/g')
+
 
 # If nothing is selected, quit.
 if [ -z "$selection" ]; then
@@ -16,17 +19,13 @@ fi
 jsonOutput=$(curl https://api.openai.com/v1/engines/text-davinci-002/completions \
                   -H "Authorization: Bearer $OPENAI_API_KEY" \
                   -H "Content-Type: application/json" \
-                  -d "{\"prompt\": \"Correct this:\\n\\n$selection\\nCorrected:\",\"max_tokens\": 3000,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0}" 2> /dev/null)
+                  -d "{\"prompt\": \"Correct this:\\n\\n${selection//$'\n'/\\n}\\nCorrected:\",\"max_tokens\": 3000,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0}" 2> /dev/null)
 
 # Extract the result from jsonOutput.
-result=$(echo $jsonOutput | jq -r '.choices[0].text')
-echo $result
+result=$(echo "$jsonOutput" | jq -r '.choices[0].text')
 
 # Remove leading carriage returns.
-result=$(echo $result | sed 's/^\^//g')
-
-# Escape result for xdotool
-result=$(echo $result | sed 's/\(["\]\)/\\\1/g')
+result=$(echo "$result" | sed 's/^\^//g')
 
 # Paste using xdotool.
 xdotool type --delay 25 --clearmodifiers "$result"
