a56c68298abf893defac0e9753768590eb82d773
Mika Pi <mika@mika.global>
2022-05-25 17:51:17 -0700

    Add C++ to the prompt

--- code-correction.sh
+++ code-correction.sh
@@ -18,10 +18,13 @@ fi
 jsonOutput=$(curl https://api.openai.com/v1/engines/code-davinci-002/completions \
                   -H "Authorization: Bearer $OPENAI_API_KEY" \
                   -H "Content-Type: application/json" \
-                  -d "{\"prompt\": \"Correct code if needed:\\n\\n${selection//$'\n'/\\n}\\nCorrected:\",\"max_tokens\": 3000,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0,\"stop\": [\"*/\"]}" 2> /dev/null)
+                  -d "{\"prompt\": \"Correct C++ code if needed:\\n\\n${selection//$'\n'/\\n}\\nCorrected:\",\"max_tokens\": 3000,\"temperature\": 0.0,\"top_p\": 1.0,\"frequency_penalty\": 0.0,\"presence_penalty\":0.0,\"stop\": [\"*/\",\"Correct C++ code if needed\"]}" 2> /dev/null)
 
 # Extract the result from jsonOutput.
 result=$(echo "$jsonOutput" | jq -r '.choices[0].text')
 
+# Remove leading spaces in each line.
+result=$(echo "$result" | sed 's/^[ \t]*//')
+
 # Paste using xdotool.
-xdotool type --delay 25 --clearmodifiers "${result:2}"
+xdotool type --delay 25 --clearmodifiers "${result:1}"
